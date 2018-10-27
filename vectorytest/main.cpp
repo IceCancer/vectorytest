9c770e0bf8d34da1684d3de41c96063ef043d7ef
liye <424734726@qq.com>
2018-10-27 22:08:01 +0800

    Initial Commit

---
+++ vectorytest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vectorytest
+//
+//  Created by   李也 on 2018/10/27.
+//  Copyright © 2018年   李也. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
